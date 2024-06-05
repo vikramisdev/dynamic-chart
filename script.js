367300ee4a2d875dcae74e13cccfe63cab46b3e9
vikramisdev <vs423502@gmail.com>
2024-06-05 23:49:27 +0530

    update

--- script.js
+++ script.js
@@ -52,7 +52,8 @@ function createChart(chartType, divId, labels, label, data, backgroundColor, bor
 // listener for the chart types
 var charts = document.getElementById("chartsType");
 
-charts.addEventListener("change", function(event) {
+if(charts != null) {
+    charts.addEventListener("change", function(event) {
         var value = event.target.value.split(" ");
         if(value.length == 2) {
             chartType = value[0].toLowerCase();
@@ -60,7 +61,12 @@ charts.addEventListener("change", function(event) {
         else if(value.length == 3) {
             chartType = value[0].toLowerCase() + value[1];
         }
+        
+        generateChart();
     });
+}
+
+
 // end
 
 
@@ -69,14 +75,12 @@ function generateChart() {
     var mLabels = document.querySelector(".labels");
     var labels = mLabels == null? " " : mLabels.value.split(" ");
     
-    var mLabel = document.querySelector(".label");
-    var label = mLabel == null? " " : mLabel.value;
+    var mTitle = document.querySelector(".title");
+    var title = mTitle == null? " " : mTitle.value;
     
     var mData = document.querySelector(".data");
     var data = mData == null? " " : mData.value.split(" ");
     
-    var mBackgroundColor = document.querySelector(".background-color");
-    var backgroundColor = mBackgroundColor == null? " " : mBackgroundColor.value;
     
     var mBorderColor = document.querySelector(".border-color");
     var borderColor = mBorderColor == null? " " : mBorderColor.value;
@@ -85,7 +89,10 @@ function generateChart() {
         chart.destroy();
     }
     
-   createChart(chartType, canvasChartId, labels, label, data, backgroundColor, borderColor);
+    var backgroundColor = generateRandomColors(data.length);
+    
+    
+   createChart(chartType, canvasChartId, labels, title, data, backgroundColor, borderColor);
 }
 
 // end
@@ -93,3 +100,19 @@ function generateChart() {
 
 // create a default bar chart at the start
 createChart(chartType, canvasChartId, ["No", "Yes"], "Are you doing good ?!", [3, 23]);
+function getRandomColor() {
+            const letters = '0123456789ABCDEF';
+            let color = '#';
+            for (let i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }
+
+        function generateRandomColors(n) {
+            const colors = [];
+            for (let i = 0; i < n; i++) {
+                colors.push(getRandomColor());
+            }
+            return colors;
+        }
